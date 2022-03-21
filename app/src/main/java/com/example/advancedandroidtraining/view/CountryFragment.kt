897c5236e65819a7ca829c7e8ff064b480e05cf4
dgencay <deniz.gencay@nucleo.com.tr>
2022-03-21 19:30:28 +0300

    first commit

---
+++ app/src/main/java/com/example/advancedandroidtraining/view/CountryFragment.kt
@@ -0,0 +1,19 @@
+package com.example.advancedandroidtraining.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.advancedandroidtraining.R
+
+class CountryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_country, container, false)
+    }
+
+}
